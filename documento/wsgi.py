20b3e8301f0702643c062ea2fb303d4f1af4adb6
Jonathan Weth <git@jonathanweth.de>
2020-09-20 20:03:56 +0200

    Reformat

--- documento/wsgi.py
+++ documento/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'documento.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "documento.settings")
 
 application = get_wsgi_application()
